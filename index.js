0a95f35b70cf96cd0231e4550750a0e50504d732
tinyTim567 <tmck911@gmail.com>
2024-11-02 20:49:36 +0000

    Fix map display

--- index.js
+++ index.js
@@ -114,3 +114,27 @@ const addRegionList = (the_list) => {
 window.addEventListener("load", () => {
     addRegionList(continents);
 });
+
+const key = 'qkOKp14TlTpS6tZnCYBN';
+const map = L.map('map').setView([0, 0], 1);
+const baseLayer = L.maptilerLayer({
+    apiKey: key,
+    style: "f60f30f5-cbe5-4499-8672-25dc30a2a5d1",
+}).addTo(map);
+
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+var admin_layer = L.geoJSON().addTo(map);
+admin_layer.addData(geojsonFeature);
